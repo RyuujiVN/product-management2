d7a801d825202f523441eb5ad501c804b3f33016
Nguyen Bao Long <161118866+nguyenbaolong145@users.noreply.github.com>
2024-11-17 15:27:12 +0700

    Làm trang chi tiết sản phẩm bên client

--- controllers/client/product-controller.js
+++ controllers/client/product-controller.js
@@ -1,18 +1,40 @@
 const Product = require('../../models/product.model');
 
-
+// [GET]    /products
 module.exports.home = async (req, res) => {
     const products = await Product.find({
         status: "active",
         deleted: false
     }).sort({ position: "desc" });
+
     const newProducts = products.map((item) => {
         item.priceNew = (item.price - item.price * item.discountPercentage / 100).toFixed(0);
         return item;
     });
-    console.log(products);
+
+    console.log(newProducts.length);
+
     res.render('client/pages/products/index', {
         pageTitle: "Danh sách sản phẩm",
         products: newProducts
     });
 };
+
+// [GET]    /products
+module.exports.detail = async (req, res) => {
+    try {
+        const find = {
+            deleted: false,
+            slug: req.params.slug,
+            status: "active"
+        }
+        const product = await Product.findOne(find);
+
+        res.render("client/pages/products/detail", {
+            pageTitle: product.title,
+            product: product
+        });
+    } catch (error) {
+        res.redirect("/products");
+    }
+}
