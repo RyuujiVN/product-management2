cdc41c23c202527ef45153e0583508f39048c886
Nguyen Bao Long <161118866+nguyenbaolong145@users.noreply.github.com>
2024-12-01 18:03:03 +0700

    Làm phân quyền

--- middleware/admin/authen-middleware.js
+++ middleware/admin/authen-middleware.js
@@ -1,6 +1,7 @@
 const systemConfig = require("../../config/system");
 
 const Account = require("../../models/accounts.model");
+const Role = require("../../models/roles.model");
 
 module.exports.requireAuthen = async (req, res, next) => {
     const token = req.cookies.token;
@@ -8,11 +9,15 @@ module.exports.requireAuthen = async (req, res, next) => {
         res.redirect(`${systemConfig.prefixAdmin}/authen/login`);
     }
     else {
-        const user = await Account.findOne({ token: token });
+        const user = await Account.findOne({ token: token }).select("-password");
         if (!user) {
             res.redirect(`${systemConfig.prefixAdmin}/authen/login`);
         }
         else {
+            const role = await Role.findOne({ _id: user.role_id }).select("title permissions");
+
+            res.locals.user = user;
+            res.locals.role = role;
             next();
         }
     }
